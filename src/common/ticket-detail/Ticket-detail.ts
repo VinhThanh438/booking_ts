e7f42a18106eb1f9a471f516953bacd4e2c49b2c
Thanh Vinh <vuthanhvinh438@gmail.gmailcom>
2024-07-31 00:15:25 +0700

    no message

--- src/common/ticket-detail/Ticket-detail.ts
+++ src/common/ticket-detail/Ticket-detail.ts
@@ -1,8 +1,8 @@
 import { ITimestamp } from '@common/timestamp.interface';
-import mongoose, { Schema, model } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-export enum StatusConstant {
-    booked = 'booked'
+export enum Status {
+    BOOKED = 'booked'
 }
 
 export interface ITicketDetailReponse {
@@ -16,7 +16,7 @@ export interface ITicketDetail extends Document, ITimestamp {
     _id: Schema.Types.ObjectId;
     ticket_name: string;
     user_name: string;
-    status: string;
+    status: Status;
 
     transform(): ITicketDetailReponse;
 }
@@ -25,7 +25,7 @@ const TicketDetailSchema = new Schema<ITicketDetail>(
     {
         ticket_name: { type: String, require: true },
         user_name: { type: String, require: true },
-        status: StatusConstant.booked
+        status: { type: String, enum: Object.values(Status), default: Status.BOOKED }
     },
     {
         timestamps: {
@@ -41,7 +41,7 @@ TicketDetailSchema.method({
             td_id: this._id.toHexString(),
             ticket_name: this.ticket_name,
             user_name: this.user_name,
-            status: this.status,
+            status: this.status
         };
 
         return transformed;
