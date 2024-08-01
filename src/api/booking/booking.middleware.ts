2dca18b162d08867a58b6098e223f63a8fa356da
thanhvinh <vuthanhvinh438@gmail.com>
2024-08-01 10:02:17 +0700

    add quantity checking middleware

---
+++ src/api/booking/booking.middleware.ts
@@ -0,0 +1,25 @@
+import logger from '@common/logger';
+import Ticket from '@common/ticket/Ticket';
+import { StatusCode } from '@config/status-code';
+import { Request, Response, NextFunction } from 'express';
+
+export class BookingMiddleware {
+    static async checkQuantity(
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        try {
+            const ticketId = req.body.ticketId
+            const data = await Ticket.findOne({ _id: ticketId, quantity: { $lte: 0 }})
+            if (data) {
+                res.status(StatusCode.REQUEST_FORBIDDEN).json({
+                    message: 'tickets are sold out'
+                })
+            } else next()
+        } catch (error) {
+            logger.error(error);
+            next(error);
+        }
+    }
+}
--- src/api/booking/booking.route.ts
+++ src/api/booking/booking.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { BookingController } from './booking.controller';
 import { AuthMidleware } from '@api/auth/auth.middleware';
+import { BookingMiddleware } from './booking.middleware';
 const router = express.Router();
 
 // router.post('/', AuthMidleware.requireAuth, TicketDetailController.addBooking);
-router.post('/', BookingController.addBooking);
+router.post('/',BookingMiddleware.checkQuantity,  BookingController.addBooking);
 
 export default router;
